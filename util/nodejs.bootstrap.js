38c35748b4b74d9b16f821dc59d32ab606bd727a
Jaguar0625 <pjlongo@Peters-MacBook-Pro.local>
2014-01-16 23:59:32 -0500

    remove unused dependencies from node.js environment and merged output

--- util/nodejs.bootstrap.js
+++ util/nodejs.bootstrap.js
@@ -1,13 +1,6 @@
-// include closure long.js on the page
-var goog = { provide: function () { }, math: { } };
-var fs = require('fs');
-eval(fs.readFileSync('external/long.js') + '');
-
 // include sha256.js on the page
+var fs = require('fs');
 eval(fs.readFileSync('external/jssha256.js') + '');
-
-// include the (external) curve25519 library
-eval(fs.readFileSync('external/curve255.js') + '');
 
 // include the curve25519 library
 eval(fs.readFileSync('core/curve25519.js') + '');
